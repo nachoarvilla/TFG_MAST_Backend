b37f0d7e3dbbb52831e752112c7b113de97cd1c0
nachoarvilla <arvillaignacio@gmail.com>
2026-05-17 19:05:24 +0200

    quitar valores por defecto de las VE

--- backend/database.py
+++ backend/database.py
@@ -5,11 +5,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_HOST = os.getenv("DB_HOST", "db")
-DB_PORT = os.getenv("DB_PORT", "3306")
-DB_NAME = os.getenv("DB_NAME", "mast_db")
-DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "tfgnacho")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 SQLALCHEMY_DATABASE_URL = os.getenv(
     "DATABASE_URL",
